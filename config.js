2edfad38c04af138d68170f94b6f09a3bacc673d
smistry1121 <sagar.mistry@knights.ucf.edu>
2016-09-28 15:31:40 -0400

    uploading config files

--- config.js
+++ config.js
@@ -1,23 +1,58 @@
+const patientEntry = require('./models/patientEntry.js'); 
+const async = require('async'); 
 module.exports= {
 	secret: 'HaR@mb3',
-
+	objectWizard: function(entry){
+		console.log(entry);
+		if(entry.length < 1){
+			return entry;
+		}
+		var object = {
+		  "bpHigh": entry[0].dailyEntry.bpHigh,
+          "bpLow": entry[0].dailyEntry.bpLow,
+          "weight": entry[0].dailyEntry.weight,
+          "exerciseTime": entry[0].dailyEntry.exerciseTime,
+          "alcoholIntake": entry[0].dailyEntry.alcoholIntake,
+          "steps": entry[0].dailyEntry.steps,
+          "averageHR": entry[0].dailyEntry.averageHR,
+          "stressLevel": entry[0].dailyEntry.stressLevel,
+          "entryInfo": entry[0].entryInfo
+		}
+		return object;
+	},
 	sortPatients:function(results){
 		const PatientList = [];
 		var patientCount = results.rowCount;
 		for(var x = 0; x<patientCount;x++){
-			var patient = {
-				 firstName: results.rows[x].firstname,
-				 lastName: results.rows[x].lastname,
-				 pid: results.rows[x].emrid,
-				 provider: results.rows[x].providerid,
-				 sex: results.rows[x].gender,
-				 dob: results.rows[x].dob,
-				 weight: results.rows[x].weight,
-				 lastInput: {},
-				 message: results.rows[x].convoid
-			};
-			PatientList.push(patient);
+			async.series([
+				function getLatestEntry(callback) {
+					patientEntry.find({"patientID":results.rows[x].emrid}).sort({"entryInfo": -1}).limit(1).exec(function(err,entry){
+						return callback(null,entry);
+					});
+				}
+				],
+				function createPatient(err,entry){
+					console.log(results);
+					var patient = {
+						 firstName: results.rows[x].firstname,
+						 lastName: results.rows[x].lastname,
+						 pid: results.rows[x].emrid,
+						 provider: results.rows[x].providerid,
+						 sex: results.rows[x].gender,
+						 dob: results.rows[x].dob,
+						 weight: results.rows[x].weight,
+						 lastInput: entry,
+						 message: results.rows[x].convoid
+						};
+					PatientList.push(patient);
+				});
+
+				
 		}
-	return PatientList;
+		var rv = {};
+		for (var i = 0; i < PatientList.length; ++i)
+		    if (PatientList[i] !== undefined) rv[i] = PatientList[i];
+		return rv;
 	}
+
 };
