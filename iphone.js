6b888da9b6ec4f0e030cbda5b330969029dc543e
smistry1121 <sagar.mistry@knights.ucf.edu>
2016-10-31 17:48:56 -0400

    console

--- iphone.js
+++ iphone.js
@@ -140,28 +140,28 @@ app.post('/register', function(req,res){ // Make this async
 	});	
 	res.status(200).json(result);
 });
-// router.use(function(req,res,next){
-// 	 var token = req.body.token || req.query.token || req.headers['x-access-token'];
-// 	 if(token){
-// 			 	 // verifies secret and checks exp
-// 		    jwt.verify(token, app.get('secret'), function(err, decoded) {      
-// 		      if (err) {
-// 		        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-// 		      } else {
-// 		        // if everything is good, save to request for use in other routes
-// 		        req.decoded = decoded;    
-// 		        next();
-// 		      }
-// 		    });
-// 		 } else {
-// 			    // if there is no token
-// 			    // return an error
-// 			    return res.status(403).send({ 
-// 			        success: false, 
-// 			        message: 'No token provided.' 
-// 			    });
-// 	 }
-// });
+router.use(function(req,res,next){
+	 var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	 if(token){
+			 	 // verifies secret and checks exp
+		    jwt.verify(token, app.get('secret'), function(err, decoded) {      
+		      if (err) {
+		        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+		      } else {
+		        // if everything is good, save to request for use in other routes
+		        req.decoded = decoded;    
+		        next();
+		      }
+		    });
+		 } else {
+			    // if there is no token
+			    // return an error
+			    return res.status(403).send({ 
+			        success: false, 
+			        message: 'No token provided.' 
+			    });
+	 }
+});
 //-------------------------------------------|
 // Patient System Routes					 |
 // 											 |
--- webserver.js
+++ webserver.js
@@ -224,6 +224,7 @@ router.get('/patientList:id:doc',function(req,res){  // Get list of Patients bas
 	});
 });
 router.post('/patient/submitData', function(req,res){   //Patient Submitting Daily Entry from their iPhone
+	console.log(req);
 	var entry = new patientEntry({
 		patientID : req.body.patientID,
 		entryInfo : moment().format(),
