fd8770f69a4ac607f96c6a6f829a1750e30036c8
smistry1121 <sagar.mistry@knights.ucf.edu>
2016-12-02 16:11:58 -0500

    blah

--- webserver.js
+++ webserver.js
@@ -535,6 +535,7 @@ router.post('/messages/patient/send',function(req,res){
 		client.query('SELECT convoid FROM public.patients WHERE emrid = ($1)',[req.body.emrid], function(err,result){
 			client.query('INSERT INTO public.messagecontent (convoid,message,messengerid,timestamp) VALUES ($1,$2,$3,$4)',[result.rows[0].convoid, req.body.message, req.body.emrid, moment().format()]);
 		});
+		client.query('UPDATE public.patients SET messagecount = messagecount + 1 WHERE emrid = ($1)',[req.body.emrid]);
 	});
 	res.status(200).json("Message Posted");	
 });
@@ -544,10 +545,11 @@ router.post('/messages/id',function(req,res){  // Send a Message from Web Portal
 			console.log('message posted error');
 		}
 		client.query('SELECT convoid FROM public.patients WHERE emrid = ($1)',[req.body.id], function(err,result){
-			client.query('INSERT INTO public.messagecontent1 (convoid,message,messengerid,timestamp) VALUES ($1,$2,$3,$4)',[result.rows[0].convoid, req.body.message, req.body.messenger, moment().format()],function(err,result1){
+			client.query('INSERT INTO public.messagecontent (convoid,message,messengerid,timestamp) VALUES ($1,$2,$3,$4)',[result.rows[0].convoid, req.body.message, req.body.messenger, moment().format()],function(err,result1){
 				if(err) throw err;	
 			});
 		});
+		client.query('UPDATE public.patients SET messagecount = messagecount + 1 WHERE emrid = ($1)',[req.body.emrid]);
 	});
 
 	
@@ -565,40 +567,6 @@ router.post('/messages',function(req,res){ // Get a conversation with a patient
 							client.query("SELECT * FROM public.messages WHERE patientid = ($1)",[req.body.id],function(err,res){
 								if(err) throw err; 
 						
-								return callback(null,res.rows[0].convoid,res.rows[0]);
-							});
-							client.release();
-						});
-					},
-				function getMessages(convoID, idValues, callback){
-						console.log(idValues);
-		 			 	pgbae.connect(function(err,client,done){
-						 	if(err){
-						 		console.log('get Message error: '+ err)
-						 	}
-						 	var message = [];
-						 	message.push(callback);
-						 	client.query("SELECT * FROM public.messagecontent WHERE convoid = ($1)",[convoID],function(err,data){
-						 		var newFormat = helper.formatMessages(idValues, data.rows);
-						 		res.json(newFormat);
-						 	});
-						 	client.release();
-						 });
-				}
-			]
-		);
-});
-router.get('/messages/mobile:id:token',function(req,res){ // Get a conversation with a patient
-		async.waterfall(
-			[
-				function getConversationID(callback){
-						pgbae.connect(function(err,client,done){
-							if(err){
-								console.log('get Message error: '+ err)
-							}
-							var conversationID;
-							client.query("SELECT * FROM public.messages WHERE patientid = ($1)",[req.query.id],function(err,res){
-								if(err) throw err; 
 								return callback(null,res.rows[0].convoid,res.rows[0]);
 							});
 							client.release();
